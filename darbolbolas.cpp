1c9ea2b09b9d2586363d8e19f9fc80834fcecf11
crespuig <hectorcrespuig@gmail.com>
2021-01-18 13:57:04 +0100

    metodos en la clase darbolbolas.cpp

--- darbolbolas.cpp
+++ darbolbolas.cpp
@@ -10,3 +10,30 @@ DArbolBolas::DArbolBolas(QVector<Bola*> *bolas, QWidget * parent) : QDialog(pare
     
 }
 
+int ModeloArbol::rowCount(const QModelIndex &paren){
+    if (!parent.isValid()){ //nos preguntan por la bola raiz
+        
+    }else{ // nos preguntan cuantas hijas tiene la bola dada
+        void *pPadre = parent.internalPointer();
+        Bola * bolaPadre = static_cast<Bola *> (pPadre);    
+        return bolaPadre->hijas.size();
+    }
+}
+
+int ModeloArbol:: columnCount(const QModelIndex &parent){
+    
+    return 1;
+    
+}
+        
+QVariant ModeloArbol::data(const QModelIndex &index, int role){
+
+}
+        
+QMoldeIndex ModeloArbol::index(int row, int column, const QModelIndex &parent){
+
+}
+        
+QModelIndex ModeloArbol::parent(const QModelIndex &index){
+
+}
